ec0f3dbeb989b237387151b53aae434ab0ac7251
nitinbuzz <reachnitinvenkat@gmail.com>
2019-01-29 17:06:59 +0000

    css tweaks

--- client/src/index.js
+++ client/src/index.js
@@ -8,9 +8,10 @@ import "./index.css";
 function App() {
   return (
     <div className="main">
+    <p className="top-text">This is KLU World Wide Chat App.</p>
+    <p className="top-right-text">No Spam || Save important data, chat will be flushed soon.</p>
       <div className="AppTitle">
-        <h1>Mr. World Wide</h1>
-        <h2>Bring it on...!</h2>
+        <h2 className="index-title">I'm Mr. KLU World Wide</h2>
       </div>
       <div>
         <Chatter messages={[]} name="nitin" endpoint="http://142.93.215.49:5000" />
