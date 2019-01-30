412f1d9d0c37e01b65708f733a4e853a069aaff5
nitinbuzz <reachnitinvenkat@gmail.com>
2019-01-30 05:41:29 +0000

    make it better

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ io.on("connection", socket => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   const path = require('path');
-  server.get('*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 //    server.use((req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
